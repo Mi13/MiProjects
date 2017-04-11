377036e333c7e6f3f974771b6d5acc2643c7a0ce
Ankit13kum <ankit.kumar@pepperfry.com>
2017-04-11 23:19:26 +0530

    validation

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ app.post('/calculateMultiplies', function(request, response) {
 
 	if ( isNaN(num) ) 
     {
-        response.json({"error": "not a valid number"});
+        response.json({"error": "not a valid number."});
     }
-    else if ( num < 3 ){
- 		response.json({"error": "number should be >= 3"});
+    else if ( num < 4 )
+    {
+ 		response.json({"error": "number atleast > 3."});
     }
     else
     {
